862ffd549ab4d0b57c8bd0f14b88ec9a56aaab00
0xhawk <hawk@horizonx.tech>
2022-11-24 23:33:53 +0800

    display posts

--- x/minioracle/keeper/grpc_query_posts.go
+++ x/minioracle/keeper/grpc_query_posts.go
@@ -2,22 +2,39 @@ package keeper
 
 import (
 	"context"
+	"minioracle/x/minioracle/types"
 
-    "minioracle/x/minioracle/types"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (k Keeper) Posts(goCtx context.Context,  req *types.QueryPostsRequest) (*types.QueryPostsResponse, error) {
+func (k Keeper) Posts(c context.Context, req *types.QueryPostsRequest) (*types.QueryPostsResponse, error) {
 	if req == nil {
-        return nil, status.Error(codes.InvalidArgument, "invalid request")
-    }
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
 
-	ctx := sdk.UnwrapSDKContext(goCtx)
+	var posts []*types.Post
 
-    // TODO: Process the query
-    _ = ctx
+	ctx := sdk.UnwrapSDKContext(c)
 
-	return &types.QueryPostsResponse{}, nil
+	store := ctx.KVStore(k.storeKey)
+
+	postStore := prefix.NewStore(store, []byte(types.PostKey))
+
+	pageRes, err := query.Paginate(postStore, req.Pagination, func(key []byte, value []byte) error {
+		var post types.Post
+		if err := k.cdc.Unmarshal(value, &post); err != nil {
+			return err
+		}
+		posts = append(posts, &post)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &types.QueryPostsResponse{Post: posts, Pagination: pageRes}, nil
 }
